c7aa85e5a42eba0a9f3ce5c5ea57fc5ac1402489
1317578865 <1317578865@qq.com>
2019-07-10 16:44:13 +0800

    1.1

--- python_cookbook/1_data_structure_and_algorithm/1.1_解压序列赋值给多个变量.py
+++ python_cookbook/1_data_structure_and_algorithm/1.1_解压序列赋值给多个变量.py
@@ -10,3 +10,15 @@ print(x, y)
 data = ['Bob', 50, 91.1, (2019, 7, 10)]
 name, shares, price, (year, month, day) = data
 print(name, shares, price, year, month, day)
+
+'''
+实际上，这种解压赋值可以用在任何可迭代对象上面，
+而不仅仅是列表或者元组。 包括字符串，文件对象，迭代器和生成器。
+'''
+
+s = "hello"
+a,b,c,d,e = s
+print(a,b,c,d,e)
+
+_,a,b,_,_ = s
+print(a,b)
